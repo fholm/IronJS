ee148917b94a255aa7ac8966d5c6ff111d43e720
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-01-11 08:40:57 +0100

    Added continue node

---
+++ IronJS/Compiler/Ast/ContinueNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronJS.Compiler.Ast
+{
+    class ContinueNode : Node
+    {
+        public ContinueNode()
+            : base(NodeType.Continue)
+        {
+
+        }
+
+        public override System.Linq.Expressions.Expression Walk(EtGenerator etgen)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

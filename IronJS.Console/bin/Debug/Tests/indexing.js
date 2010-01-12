31bcd6b4dfc5b5c94fc6a7ceda159a55cdf3ca2a
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-01-12 17:33:47 +0100

    Renamed IronJS.Testing to IronJS.Console

---
+++ IronJS.Console/bin/Debug/Tests/indexing.js
@@ -0,0 +1,15 @@
+﻿
+foo = { bar: { x: "x" }, y: "y" };
+
+print(foo);
+print(foo["y"]);
+print(foo["bar"]);
+print(foo["bar"]["x"]);
+print(foo.bar["x"]);
+
+
+foo["bar"]["x"] = "new x";
+print(foo["bar"]["x"]);
+
+foo["bar"] = { hello: "world" };
+print(foo["bar"].hello);
---
+++ IronJS.Console/bin/Debug/Tests/try_catch_finally.js
@@ -0,0 +1,24 @@
+﻿try {
+    throw exc;
+} catch (e) {
+    print("exception caught");
+    print("try ... catch");
+}
+
+print("--------------------------");
+
+try {
+    throw exc;
+} catch (e) {
+    print("exception caught");
+} finally {
+    print("try ... catch ... finally");
+}
+
+print("--------------------------");
+
+try {
+    //throw exc;
+} finally {
+    print("try ... finally");
+} 

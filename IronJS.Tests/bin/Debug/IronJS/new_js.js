eceb9ccb308d65816afae51f81ab34390fe5cf67
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-01-12 17:45:44 +0100

    Accidently added IronJS.tests/obj and /bin

---
+++ IronJS.Tests/bin/Debug/IronJS/new_js.js
@@ -0,0 +1,22 @@
+﻿function foo_bar_baz(as) {
+    this.as = as;
+    this.built = "from baz";
+};
+
+function foo_bar(as) {
+    this.built = "from bar";
+    this.as = as;
+    return { baz: foo_bar_baz };
+};
+
+foo = {}
+foo.bar = foo_bar;
+foo.bar.baz = foo_bar_baz;
+
+var obj = new (foo.bar().baz)("hello world");
+print(obj.built);
+print(obj.as);
+
+var obj2 = new foo.bar.baz("lol");
+print(obj2.built);
+print(obj2.as);
---
+++ IronJS.Tests/bin/Debug/IronJS/with.js
@@ -0,0 +1,46 @@
+﻿
+function f() {
+    print(this.x);
+}
+
+foo = { f: f, x: "foo.x", y: "foo.y" }
+bar = { f: f, x: "bar.x", foo: foo }
+
+with (foo) {
+
+    print(bar);
+    print(foo);
+    print(x);
+
+    with (bar) {
+
+        aaa = function () {
+            print("aaa: " + x);
+        };
+
+        var bbb = function () {
+            print("bbb: " + foo.x);
+        };
+
+        print(y);
+
+        f();
+
+        print(foo.x);
+
+        print(bar.foo.x);
+
+    }
+
+    f();
+
+    x = "foo.x (with)";
+
+    f();
+
+}
+
+print(foo.x);
+print(bar.x);
+aaa();
+bbb();

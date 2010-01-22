7296299bd053726588b6690cdcbc5ffc6a16b221
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-01-22 19:06:55 +0100

    Added tests for unicode variable names, minor changes to Function/Property

---
+++ IronJS.Tests/VariableTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronJS.Tests
+{
+    [TestClass]
+    public class VariableTests
+    {
+        [TestMethod]
+        public void TestVariablesUnicodeNamesAreOk()
+        {
+            Assert.AreEqual(
+                "foo",
+                ScriptRunner.Run(
+                    "åäö = 'foo'; emit(åäö);"
+                )
+            );
+        }
+    }
+}

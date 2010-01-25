1aac35f4b3dfe93ab2b73175796afe3140a81e88
unknown <fredrik.johan.holmstrom@gmail.com>
2010-01-25 08:44:46 +0100

    Added new IConstructor interface that allows splitting up IFunction into two interfaces: One for [[Call]] and one for [[Construct]] and [[HasInstance]]. Also uppdated iobj-inheritance diagram again

---
+++ IronJS/Runtime/Js/IConstructor.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace IronJS.Runtime.Js
+{
+    interface IConstructor : IFunction
+    {
+        IObj Construct(object[] args);  // [[Construct]]
+        bool HasInstance(object obj);   // [[HasInstance]]
+    }
+
+    static public class IConstructorMethods
+    {
+        static public readonly MethodInfo MiConstruct = typeof(IFunction).GetMethod("Construct");
+        static public readonly MethodInfo MiHasInstance = typeof(IFunction).GetMethod("HasInstance");
+    }
+}

bac58e11c5df7df2fa449849e194b39c7831bebe
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-03-18 07:56:28 +0100

    Additions to Type structure

--- Src/IronJS/Module1.fs
+++ Src/IronJS/Module1.fs
@@ -2,6 +2,18 @@
 
 module Module1
 
+type Number =
+  | Double of double
+  | Integer of int64
+
+type Variable =
+  | Enclosed of string
+  | Local of string
+  | Parameter of string
+
 type Node =
   | Symbol of string
   | String of string
+  | Number of Number
+  | Variable of Variable
+

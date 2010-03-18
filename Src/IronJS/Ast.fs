1b3d8814b103f8b2b87c178ff6aa2ab09430d732
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-03-18 19:15:27 +0100

    Minor changes

--- Src/IronJS/Ast.fs
+++ Src/IronJS/Ast.fs
@@ -1,31 +1,35 @@
 ﻿// Learn more about F# at http://fsharp.net
 
-module Module1
+module Ast
 
 type Number =
   | Double of double
   | Integer of int64
 
-type Variable =
+type VarType =
   | Enclosed of string
   | Local of string
   | Parameter of string
 
 type BinaryOp =
-  | Add
-  | Sub
+  | Add = 0
+  | Sub = 1
+  | Div = 2
+  | Mul = 3
+  | Mod = 4
 
 type UnaryOp =
-  | Void
-  | Delete
+  | Void = 0
+  | Delete = 1
 
 type Node =
   | Symbol of string
   | String of string
   | Number of Number
-  | Variable of Variable
+  | Variable of VarType
   | If of Node * Node * Node
   | Function of Node * Node list * Node
   | Binary of BinaryOp * Node * Node
   | Unary of UnaryOp * Node
-  | Null
+  | Invoke of Node * Node list
+  | Null 

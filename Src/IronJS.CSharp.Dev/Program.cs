32bd1077e86226ae084a9ff190670451b4833916
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-03-26 16:51:56 +0100

    Added test-project for CSharp code

---
+++ Src/IronJS.CSharp.Dev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronJS.CSharp.Dev {
+	class Program {
+		static void Main(string[] args) {
+			var test1 = new object[2];
+			test1[0] = typeof(int);
+			test1[1] = typeof(string);
+
+			var test2 = new object[2];
+			test2[0] = typeof(int);
+			test2[1] = typeof(string);
+
+			Console.WriteLine(test1[0].GetHashCode() ^ test1[1].GetHashCode());
+			Console.WriteLine(test2[0].GetHashCode() ^ test2[1].GetHashCode());
+
+			Console.ReadLine();
+		}
+	}
+}

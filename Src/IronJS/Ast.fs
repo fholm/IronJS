cf7a6d3e4590f7829a6d0a28ea2f9e0ce6062e1d
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-03-28 10:18:22 +0200

    backup point before removing type detection in AST phase

--- Src/IronJS/Ast.fs
+++ Src/IronJS/Ast.fs
@@ -18,9 +18,13 @@ type Local = {
   ParamIndex: int
 }
 
+type Closure = {
+  Index: int
+}
+
 type Scope = {
   Locals: Map<string, Local>
-  Closure: string list
+  Closure: Map<string, Closure>
 }
 
 type Node =
@@ -48,12 +52,12 @@ type private Generator = CommonTree -> Scopes -> Node
 type private GeneratorMap = Map<int, CommonTree -> Scopes -> Generator -> Node>
 
 //Constants
-let internal newScope = { 
-  Locals = Map.empty;
-  Closure = [];
+let private newScope = { 
+  Locals  = Map.empty;
+  Closure = Map.empty;
 }
 
-let internal newLocal = {
+let private newLocal = {
   UsedWith = Set.empty;
   UsedAs = Types.JsTypes.None;
   ForcedType = None;
@@ -67,58 +71,36 @@ let private child (tree:AstTree) index = (ct tree.Children.[index])
 let private children (tree:AstTree) = toList<AstTree> tree.Children
 let private isAssign (tree:AstTree) = tree.Type = ES3Parser.ASSIGN
 let private isAnonymous (tree:AstTree) = tree.Type = ES3Parser.FUNCTION && tree.ChildCount = 2
-let private getParameterNames (tree:AstTree) = [for c in (children (child tree 0)) -> c.Text]
 let private hasLocal (scope:Scope) name = scope.Locals.ContainsKey name
+let private setLocal (scope:Scope) (name:string) (loc:Local) = { scope with Locals = scope.Locals.Add(name, loc) }
 let private addUsedWith (loc:Local) (name:string) = { loc with UsedWith = Set.add name loc.UsedWith }
 let private addUsedAs (loc:Local) (typ:Types.JsTypes) = { loc with UsedAs = typ ||| loc.UsedAs }
-let private setLocal (scope:Scope) (name:string) (loc:Local) = { scope with Locals = scope.Locals.Add(name, loc) }
-let private getClosureIndex (scope:Scope) (name:string) = indexOf scope.Closure name
-let private setClosedOver (scope:Scope) (name:string) = setLocal scope name { scope.Locals.[name] with ClosedOver = true }
-
-let private addClosure (scope:Scope) (name:string) =
-  if List.exists (fun var -> var = name) scope.Closure 
-    then scope
-    else { scope with Closure = name :: scope.Closure } 
+let private setClosure (scope:Scope) (name:string) (clos:Closure) = { scope with Closure = scope.Closure.Add(name, clos) }
+let private cleanString = function | null | "" -> "" | s  -> if s.[0] = '"' then s.Trim('"') else s.Trim('\'')
 
 let private exprType = function
   | Number(_) -> Types.JsTypes.Double
   | String(_) -> Types.JsTypes.String
   | _ -> Types.JsTypes.Dynamic
 
-let private cleanString = function 
-  | null | "" -> "" 
-  | s  -> if s.[0] = '"' then s.Trim('"') else s.Trim('\'')
+let private createClosure (scope:Scope) name = 
+  if scope.Closure.ContainsKey name 
+    then scope 
+    else setClosure scope name { Index = scope.Closure.Count }
 
-let private addLocal (scopes:Scopes) (name:string) =
+let private createLocal (scopes:Scopes) (name:string) =
   match !scopes with
   | [] -> ()
-  | x::xs -> scopes := { x with Locals = x.Locals.Add(name, newLocal) } :: xs
+  | scope::xs -> scopes := setLocal scope name newLocal :: xs
 
 let private getVariable (scopes:Scopes) (name:string) =
-
   match !scopes with
   | [] -> Global(name)
   | scope::xs when hasLocal scope name -> Local(name)
-  | _ -> 
-    Pass
-    (*
-    let rec findClosedOverLocal (scopes:Scope list) =
-      match scopes with
-      | [] -> false, []
-      | scope::scopes ->
-        if hasLocal scope name then
-          true, (setClosedOver scope name) :: scopes
-        else
-          let found, scopes = findLocal scopes
-          found, (if found then addClosure scope name else scope) :: scopes
-
-    let found, newScopes = findClosedOverLocal !scopes
-    scopes := newScopes
-
-    if found then Closure(name, getClosureIndex (!scopes).Head *)
+  | _ -> Pass
 
 let private createScope (tree:AstTree) =
-  let parms = getParameterNames tree
+  let parms = [for c in (children (child tree 0)) -> c.Text]
 
   let rec doAdd (parms:string list) (locals:Map<string, Local>) (n:int) =
     match parms with
@@ -127,18 +109,17 @@ let private createScope (tree:AstTree) =
 
   { newScope with Locals = (doAdd parms Map.empty 0) }
 
+let private addLocalTypeData (loc:Local) (node:Node) =
+  match node with
+  | Local(name) -> addUsedWith loc name
+  | _ -> addUsedAs loc (exprType node)
+
 let private addTypeData (left:Node) (right:Node) (scopes:Scopes) =
   match !scopes with 
   | [] -> ()
   | scope::xs ->
     match left with
-    | Local(name) -> 
-      let local = scope.Locals.[name]
-      let modified = match right with
-                     | Local(name) -> addUsedWith local name
-                     | _ -> addUsedAs local (exprType right)
-
-      scopes := setLocal scope name modified :: xs 
+    | Local(name) -> scopes := setLocal scope name (addLocalTypeData scope.Locals.[name] right) :: xs 
     | _ -> ()
 
 //Default Generators
@@ -152,12 +133,15 @@ let defaultGenerators =
     (ES3Parser.DecimalLiteral, fun tree _ _  -> Number(double tree.Text));
     (ES3Parser.RETURN, fun tree scopes gen -> Return(gen (child tree 0) scopes));
 
-    (ES3Parser.VAR, fun tree scopes gen ->
+    (ES3Parser.VAR, fun tree scopes gen -> 
       let child0 = child tree 0
-      let name   = (if isAssign child0 then child child0 0 else child0).Text
 
-      addLocal scopes name
-      gen child0 scopes
+      if isAssign child0 then 
+        createLocal scopes (child child0 0).Text
+        gen child0 scopes
+      else 
+        createLocal scopes child0.Text
+        Pass
     );
 
     (ES3Parser.ASSIGN, fun tree scopes gen ->
--- Src/IronJS/Testing.js
+++ Src/IronJS/Testing.js
@@ -3,6 +3,7 @@ var y = 2;
 var z = "foo";
 var foo = function(a1, a2) {
     var bar = 2;
+    bar = a1;
     return function() {
         return bar;
     };

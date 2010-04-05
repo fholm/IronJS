da05a101a91945d561c7385efc07e68e42d6c440
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-04-05 10:45:48 +0200

    New DelegateCell

--- Src/IronJS/Constants.fs
+++ Src/IronJS/Constants.fs
@@ -1,8 +1,17 @@
 ﻿module IronJS.Constants
 
 let maxTypedArgs  = 125
-let clrDynamic    = typeof<obj>
-let clrDouble     = typeof<double>
-let clrString     = typeof<string>
-let clrVoid       = typeof<System.Void>
+
+let clrDynamic = typeof<obj>
+let clrDynamicHashCode = clrDynamic.GetHashCode()
+
+let clrDouble = typeof<double>
+let clrDoubleHashCode = clrDouble.GetHashCode()
+
+let clrString = typeof<string>
+let clrStringHashCode = clrString.GetHashCode()
+
+let clrVoid = typeof<System.Void>
+let clrVoidHashCode = clrVoid.GetHashCode()
+
 let strongBoxTypeDef = typedefof<System.Runtime.CompilerServices.StrongBox<_>>

8f095479084090e081ced9eb748b02816530a196
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-04-11 23:34:44 +0200

    Titied up Utils.fs

--- Src/IronJS/Utils.fs
+++ Src/IronJS/Utils.fs
@@ -2,21 +2,29 @@
 
 open System
 
-//Public Type Aliases
-type MetaObj = System.Dynamic.DynamicMetaObject
-type Et = System.Linq.Expressions.Expression
-type EtParam = System.Linq.Expressions.ParameterExpression
-type EtLambda = System.Linq.Expressions.LambdaExpression
-type AstUtils = Microsoft.Scripting.Ast.Utils
-type JitCache = System.Collections.Concurrent.ConcurrentDictionary<System.Type, System.Delegate>
-type CtorInfo = System.Reflection.ConstructorInfo
-type ParmInfo = System.Reflection.ParameterInfo
-type AstTree = Antlr.Runtime.Tree.CommonTree
-type StrongBox<'a> = System.Runtime.CompilerServices.StrongBox<'a>
-type ClrType = System.Type
-type Dynamic = obj
+(*Public Type Aliases*)
+type MetaObj        = System.Dynamic.DynamicMetaObject
 
-//Functions
+type Et             = System.Linq.Expressions.Expression
+type EtParam        = System.Linq.Expressions.ParameterExpression
+type EtLambda       = System.Linq.Expressions.LambdaExpression
+
+type AstUtils       = Microsoft.Scripting.Ast.Utils
+type DynamicUtils   = Microsoft.Scripting.Utils.DynamicUtils
+
+type CtorInfo       = System.Reflection.ConstructorInfo
+type ParmInfo       = System.Reflection.ParameterInfo
+type FieldInfo      = System.Reflection.FieldInfo
+type MethodInfo     = System.Reflection.MethodInfo
+
+type AstTree        = Antlr.Runtime.Tree.CommonTree
+
+type ClrType        = System.Type
+type Dynamic        = System.Object
+type StrongBox<'a>  = System.Runtime.CompilerServices.StrongBox<'a>
+type JitCache       = System.Collections.Concurrent.ConcurrentDictionary<System.Type, System.Delegate>
+
+
 let toList<'a> (ilst:System.Collections.IList) =
   match ilst with
   | null -> []

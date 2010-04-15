3629646cee02c12247b17bf9777c90de7401a519
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-04-15 07:28:38 +0200

    minor changes

--- Src/IronJS/Runtime.Helpers.Variables.fs
+++ Src/IronJS/Runtime.Helpers.Variables.fs
@@ -8,18 +8,18 @@ open IronJS.Runtime
 type ObjectList = Object ResizeArray
 
 module Variables = 
-  let rec private scanObjects fnc (lst:Scope ResizeArray) topScope = 
-    let rec scanObjects n = 
+  let rec private scanScopes fnc (lst:Scope ResizeArray) topScope = 
+    let rec scanScopes n = 
       if n >= lst.Count
         then  false, null
         else  let scope = lst.[n]
               if scope.ScopeLevel < topScope 
                 then  false, null
-                else  let pair = fnc lst.[n] 
-                      if (fst pair) 
+                else  let pair = fnc scope
+                      if fst pair
                         then pair
-                        else scanObjects (n+1)
-    scanObjects 0
+                        else scanScopes (n+1)
+    scanScopes 0
 
   let private setInObjects (name:string) (value:Dynamic) scopes = 
     if scopes = null
@@ -52,7 +52,7 @@ module Variables =
       let pair = getFromObjects name localScopes
       if (fst pair)
         then  pair
-        else  let pair = scanObjects (fun (x:Scope) -> getFromObjects name x.Objects) closure.Scopes maxScopeLevel
+        else  let pair = scanScopes (fun (x:Scope) -> getFromObjects name x.Objects) closure.Scopes maxScopeLevel
               if (fst pair)
                 then pair
                 else false, null
@@ -60,7 +60,7 @@ module Variables =
     static member Set(name:string, value:Dynamic, localScopes:ObjectList, closure:Closure, maxScopeLevel:int) = 
       if setInObjects name value localScopes
         then  true
-        else  let found, _ = scanObjects (fun (x:Scope) -> setInObjects name value x.Objects, null) closure.Scopes maxScopeLevel
+        else  let found, _ = scanScopes (fun (x:Scope) -> setInObjects name value x.Objects, null) closure.Scopes maxScopeLevel
               found
 
   (**)
@@ -69,7 +69,7 @@ module Variables =
       let found, item = getFromObjects name localScopes
       if found 
         then  item
-        else  let found, item = scanObjects (fun (x:Scope) -> getFromObjects name x.Objects) closure.Scopes (-1)
+        else  let found, item = scanScopes (fun (x:Scope) -> getFromObjects name x.Objects) closure.Scopes (-1)
               if found 
                 then item
                 else closure.Globals.Get name

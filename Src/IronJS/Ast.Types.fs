9904ccdbae577903f5d958b275cc540e04dd903e
Fredrik Holmstrm <fredrik.johan.holmstrom@gmail.com>
2010-04-16 19:58:41 +0200

    minor changes

--- Src/IronJS/Ast.Types.fs
+++ Src/IronJS/Ast.Types.fs
@@ -6,77 +6,77 @@ open Antlr.Runtime.Tree
 open System.Diagnostics
 
 type JsTypes = 
-  | Nothing   = 0
-  | Double    = 1
-  | Integer   = 2 // Not used
-  | String    = 4
-  | Object    = 8
-  | Dynamic   = 16
+  | Nothing = 0
+  | Double  = 1
+  | Integer = 2 // Not used
+  | String  = 4
+  | Object  = 8
+  | Dynamic = 16
 
 [<DebuggerDisplay("{DebugView}")>]
 type Local = {
-  Expr:             EtParam
-  ParamIndex:       int
-  InitUndefined:    bool
-  ClosedOver:       bool
-  UsedAs:           JsTypes
-  UsedWith:         string Set
-  UsedWithClosure:  string Set
+  Expr: EtParam
+  ParamIndex: int
+  InitUndefined: bool
+  ClosedOver: bool
+  UsedAs: JsTypes
+  UsedWith: string Set
+  UsedWithClosure: string Set
 } with
-  member x.IsParameter  = x.ParamIndex > -1
+  member x.IsParameter = x.ParamIndex > -1
   member x.DebugView = (sprintf 
     @"closure:%b/index:%i/undefined:%b/as:%A/with:%A, %A" 
     x.ClosedOver x.ParamIndex x.InitUndefined x.UsedAs x.UsedWith x.UsedWithClosure)
   static member New = {
-    Expr            = null
-    ParamIndex      = -1
-    InitUndefined   = false
-    ClosedOver      = false
-    UsedAs          = JsTypes.Nothing
-    UsedWith        = Set.empty
+    Expr = null
+    ParamIndex = -1
+    InitUndefined = false
+    ClosedOver = false
+    UsedAs = JsTypes.Nothing
+    UsedWith = Set.empty
     UsedWithClosure = Set.empty
   }
   
 [<DebuggerDisplay("{DebugView}")>]
 type Closure = {
-  Index:                int
-  DefinedInScopeLevel:  int
+  Index: int
+  DefinedInScopeLevel: int
 } with
   member x.DebugView = sprintf "index:%i/from:%i" x.Index x.DefinedInScopeLevel
   static member New = {
-    Index               = -1
+    Index = -1
     DefinedInScopeLevel = -1
   }
 
-type LocalMap   = Map<string, Local>
+type LocalMap = Map<string, Local>
 type ClosureMap = Map<string, Closure>
 
 type Scope = {
-  Locals:             LocalMap
-  Closure:            ClosureMap
-  Arguments:          bool
-  ScopeLevel:         int
-  HasDynamicScopes:   bool
+  Locals: LocalMap
+  Closure: ClosureMap
+  Arguments: bool
+  ScopeLevel: int
+  HasDynamicScopes: bool
   InParentDynamicScope: bool
 } with
   static member New = { 
-    Locals            = Map.empty
-    Closure           = Map.empty
-    Arguments         = false
-    ScopeLevel        = 0
-    HasDynamicScopes  = false
+    Locals = Map.empty
+    Closure = Map.empty
+    Arguments = false
+    ScopeLevel = 0
+    HasDynamicScopes = false
     InParentDynamicScope = false
   }
   static member Global = Scope.New
 
 type ParserState = { 
-  ScopeChain:               Scope list
-  GlobalDynamicScopeLevel:  int
-  LocalDynamicScopeLevels:  int list
+  ScopeChain: Scope list
+  GlobalDynamicScopeLevel: int
+  LocalDynamicScopeLevels: int list
 } with
   member x.InDynamicScope = x.GlobalDynamicScopeLevel > 0
   static member New = {
-    ScopeChain              = []
+    ScopeChain = []
     GlobalDynamicScopeLevel = 0
     LocalDynamicScopeLevels = [0]
   }

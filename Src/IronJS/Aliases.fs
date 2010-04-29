49dd57f7e8b752e9d78cbe69da965086bb886d5c
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-04-29 16:53:07 +0200

    Begun stripping out the State Monad used for AST parsing

--- Src/IronJS/Aliases.fs
+++ Src/IronJS/Aliases.fs
@@ -1,23 +1,25 @@
 ﻿namespace IronJS.Aliases
 
-type Lexer  = IronJS.Parser.ES3Lexer
-type Parser = IronJS.Parser.ES3Parser
+type AntlrLexer  = IronJS.Parser.ES3Lexer
+type AntlrParser = IronJS.Parser.ES3Parser
 
-type MetaObj        = System.Dynamic.DynamicMetaObject
+type MetaObj          = System.Dynamic.DynamicMetaObject
+type IMetaObjProvider = System.Dynamic.IDynamicMetaObjectProvider
 
-type Et             = System.Linq.Expressions.Expression
-type EtParam        = System.Linq.Expressions.ParameterExpression
-type EtLambda       = System.Linq.Expressions.LambdaExpression
+type Et       = System.Linq.Expressions.Expression
+type EtParam  = System.Linq.Expressions.ParameterExpression
+type EtLambda = System.Linq.Expressions.LambdaExpression
 
-type AstUtils       = Microsoft.Scripting.Ast.Utils
-type DynamicUtils   = Microsoft.Scripting.Utils.DynamicUtils
+type AstUtils     = Microsoft.Scripting.Ast.Utils
+type DynamicUtils = Microsoft.Scripting.Utils.DynamicUtils
 
-type CtorInfo       = System.Reflection.ConstructorInfo
-type ParmInfo       = System.Reflection.ParameterInfo
-type FieldInfo      = System.Reflection.FieldInfo
-type MethodInfo     = System.Reflection.MethodInfo
+type CtorInfo   = System.Reflection.ConstructorInfo
+type ParmInfo   = System.Reflection.ParameterInfo
+type FieldInfo  = System.Reflection.FieldInfo
+type MethodInfo = System.Reflection.MethodInfo
 
-type AstTree        = Antlr.Runtime.Tree.CommonTree
+type AstTree    = Antlr.Runtime.Tree.CommonTree
+type AntlrToken = Antlr.Runtime.Tree.CommonTree
 
 type ClrType          = System.Type
 type ClrObject        = System.Object
@@ -26,4 +28,5 @@ type IEnum<'a>        = System.Collections.Generic.IEnumerable<'a>
 type Dict<'a, 'b>     = System.Collections.Generic.Dictionary<'a, 'b>
 type SafeDict<'a, 'b> = System.Collections.Concurrent.ConcurrentDictionary<'a, 'b>
 type Delegate         = System.Delegate
+
 type DebuggerDisplayAttribute = System.Diagnostics.DebuggerDisplayAttribute
--- Src/IronJS/Ast2.fs
+++ Src/IronJS/Ast2.fs
@@ -3,5 +3,52 @@
 open IronJS
 open IronJS.Aliases
 open IronJS.Tools
+open IronJS.Tools.Antlr
 open IronJS.Ast
 
+module Parser =
+
+  type private State = ParserState ref
+  
+  let rec parse (sr:State) (t:AntlrToken) =
+    match t.Type with
+    | 0 | AntlrParser.BLOCK -> parseBlock sr t
+
+  and parseList (sr:State) (tl:AntlrToken list) =
+    match tl with
+    | []    -> [] 
+    | x::xs -> parse sr x :: parseList sr xs
+
+  and parseBlock sr t =
+    Block(parseList sr (children t))
+
+  and parseExpr sr t = 
+    parse sr (child t 0)
+
+  and parseInc sr t =
+    let target = parse sr (child t 0)
+    Assign(target, BinaryOp(Add, target, Utils.intAsNode 1))
+
+  and parsePreInc sr t = 
+    UnaryOp(PreInc, parse sr (child t 0))
+
+  and parseBinary sr t op = 
+    BinaryOp(op, parse sr (child t 0), parse sr (child t 0))
+
+  and parseNumber sr (t:AntlrToken) =
+    Utils.strToNumber t.Text
+
+  and parseString sr (t:AntlrToken) = 
+    String(Utils.cleanString t.Text)
+
+  and parseObject sr (t:AntlrToken) = 
+    if t.Children = null then Object(None) else Error("Not supported")
+
+  and parseReturn sr t = 
+    Return(parse sr (child t 0))
+
+  and parseByField sr t = 
+    Property(parse sr (child t 0), (child t 1).Text)
+
+  and parsePossibleNull sr t = 
+    if t = null then Null else parse sr t

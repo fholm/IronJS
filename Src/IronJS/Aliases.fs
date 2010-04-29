f50bc01fec8b75ed91d02fbdd59b36ffca625c0f
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-04-29 20:06:55 +0200

    Renamed private type alias BindingRestriction to BR in Tools.Dlr.Restrict

--- Src/IronJS/Aliases.fs
+++ Src/IronJS/Aliases.fs
@@ -9,7 +9,6 @@ type IMetaObjProvider = System.Dynamic.IDynamicMetaObjectProvider
 type Et       = System.Linq.Expressions.Expression
 type EtParam  = System.Linq.Expressions.ParameterExpression
 type EtLambda = System.Linq.Expressions.LambdaExpression
-type Restrict = System.Dynamic.BindingRestrictions
 
 type AstUtils     = Microsoft.Scripting.Ast.Utils
 type DynamicUtils = Microsoft.Scripting.Utils.DynamicUtils
--- Src/IronJS/Tools.Dlr.Restrict.fs
+++ Src/IronJS/Tools.Dlr.Restrict.fs
@@ -7,20 +7,20 @@ open IronJS.Tools
 (*Tools for working with the System.Dynamic.BindingRestrictions class*)
 module Restrict =
 
-  type private BindingRestrictions = System.Dynamic.BindingRestrictions
+  type private BR = System.Dynamic.BindingRestrictions
   
   let notAtAll = System.Dynamic.BindingRestrictions.Empty
-  let byExpr expr = BindingRestrictions.GetExpressionRestriction(expr)
-  let byType expr typ = BindingRestrictions.GetTypeRestriction(expr, typ)
-  let byInstance expr instance = BindingRestrictions.GetInstanceRestriction(expr, instance)
+  let byExpr expr = BR.GetExpressionRestriction(expr)
+  let byType expr typ = BR.GetTypeRestriction(expr, typ)
+  let byInstance expr instance = BR.GetInstanceRestriction(expr, instance)
 
   let argRestrict (a:MetaObj) =
     let restriction = 
       if a.HasValue && a.Value = null 
-        then BindingRestrictions.GetInstanceRestriction(a.Expression, Dlr.Expr.null')
-        else BindingRestrictions.GetTypeRestriction(a.Expression, a.LimitType)
+        then BR.GetInstanceRestriction(a.Expression, Dlr.Expr.null')
+        else BR.GetTypeRestriction(a.Expression, a.LimitType)
 
     a.Restrictions.Merge(restriction)
 
   let byArgs (args:MetaObj seq) =
-    Seq.fold (fun (s:Restrict) a -> s.Merge(argRestrict a)) BindingRestrictions.Empty args
+    Seq.fold (fun (s:BR) a -> s.Merge(argRestrict a)) BR.Empty args

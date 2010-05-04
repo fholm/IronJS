bae1e2064a8cdde0130008e4e63341829d04c11b
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-05-04 07:21:12 +0200

    Simplified Runtime.Helpers.Function.Closures.BuildScopes

--- Src/IronJS/Runtime.Helpers.Function.fs
+++ Src/IronJS/Runtime.Helpers.Function.fs
@@ -6,8 +6,5 @@ open IronJS.Tools
 open IronJS.Runtime
 
 type Closures = 
-  static member BuildScopes (closure:Closure, localScopes:Object ResizeArray, scopeLevel:int) =
-    let scopes = new ResizeArray<Scope>(closure.Scopes)
-    let localScope = new Scope(List.empty, null, scopeLevel)
-    scopes.Insert(0, localScope)
-    scopes
+  static member BuildScopes (closure:Closure, evalObject, localScopes, scopeLevel) =
+    new Scope(localScopes, evalObject, scopeLevel) :: closure.Scopes

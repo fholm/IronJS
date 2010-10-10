e4e1030a9ea5c79e336499556a3051f76e56a97c
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-10-10 21:16:12 +0200

    Moved projects around

---
+++ Src/Examples/ClrMethodAsJsFunction/Program.fs
@@ -0,0 +1,23 @@
+﻿namespace IronJS.Dev
+
+module Main = 
+
+  open System
+  open IronJS
+
+  let main () =
+
+    let ctx = IronJS.Hosting.Context.Create()
+
+    let method' = 
+      Reflection.getMethodArgsT<Console> "WriteLine" [typeof<string>]
+
+    match method' with
+    | None -> failwith "Console.WriteLine(string) not found"
+    | Some methodInfo ->
+      ctx.PutGlobal(
+        "print", Api.ClrFunction.create(ctx.Environment, methodInfo))
+
+    ctx.Execute @"print('Hello World');"
+    
+  main() |> ignore
---
+++ Src/Examples/DelegateAsJsFunction/Program.fs
@@ -0,0 +1,23 @@
+﻿namespace IronJS.Dev
+
+module Main = 
+
+  open System
+  open IronJS
+
+  let main () =
+
+    let ctx = IronJS.Hosting.Context.Create()
+
+    let increment (n:IjsNum) = n + 1.0
+
+    ctx.PutGlobal("increment", 
+      Api.DelegateFunction.create(
+        ctx.Environment, new Func<IjsNum, IjsNum>(increment)
+      )
+    )
+
+    let result = ctx.ExecuteT<IjsNum> @"var i = 1; increment(i);"
+    printf "%.1f" result
+    
+  main() |> ignore

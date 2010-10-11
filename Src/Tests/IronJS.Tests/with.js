f706baf45a60f3114a2e5b82f0674db27f7070a1
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-10-11 09:40:30 +0200

    Re-added missing .js test files

---
+++ Src/Tests/IronJS.Tests/with.js
@@ -0,0 +1,27 @@
+﻿var name = "With Tests";
+
+var tests = {
+    simpleWith: function () {
+        var x = 2;
+        with ({ x: 1 }) {
+            assertEqual(x, 1, "x should equal 1");
+        }
+    },
+
+    nestedWith: function () {
+        with ({ x: "outerX", y: "outerY" }) {
+            with ({ x: "innerX" }) {
+                assertEqual(x, "innerX", "x should equal 'innerX'");
+                assertEqual(y, "outerY", "y should equal 'outerY'");
+            }
+        }
+    },
+
+    withScopeShouldBeClosedOver: function () {
+        var x = "outerX";
+        with ({ x: "withX" }) {
+            var y = function () { return x; };
+        }
+        assertEqual(y(), "withX", "y should return 'withX'");
+    }
+}

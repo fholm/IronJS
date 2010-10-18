2c2138b35ed9f342fbc68fa76df51bcd77d2c0d2
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-10-18 09:02:00 +0200

    Config changes

--- Src/Core/IronJS/Config.fs
+++ Src/Core/IronJS/Config.fs
@@ -11,37 +11,25 @@ module Version =
   let Tupled = Major, Minor, Build, Revision, Tag
   let FullName = sprintf "IronJS %s" Tagged
 
-module Operators =
-  let inline (|!>) a b = b !a 
-  let inline (<==) a b = a := b !a
-  let inline (|?>) a b = Option.map b a
-  let inline (|?) a b = match a with Some x -> x | _ -> b()
-
-module Ops = 
-  ()
-  //Same as |> but for refs
-  //let (%>) a b = b !a 
-
-  //Applies b to a and stores result in a
-  //let (<!) a b = a := b !a
-
-  //Applies a to b if it has a value and returns Some(value) else None
-  //let (>?) a b = match b with Some x -> Some(a x) | _ -> None
-
-  //'Unwraps' an Option<'a> if it has  a value otherwise throws an exception
-  //let (!?) opt = match opt with | Some v -> v | _ -> failwith "No value"
-
 module Aliases = 
   open System
+  open System.Globalization
+  open System.Collections.Generic
     
-  type MutableList<'a> = Collections.Generic.List<'a>
-  type MutableStack<'a> = Collections.Generic.Stack<'a>
-  type MutableDict<'k, 'v> = Collections.Generic.Dictionary<'k, 'v>
-  type MutableSorted<'k, 'v> = Collections.Generic.SortedDictionary<'k, 'v>
+  type MutableList<'a> = List<'a>
+  type MutableStack<'a> = Stack<'a>
+  type MutableDict<'k, 'v> = Dictionary<'k, 'v>
+  type MutableSorted<'k, 'v> = SortedDictionary<'k, 'v>
 
-  let anyNumber = Globalization.NumberStyles.Any
-  let invariantCulture = Globalization.CultureInfo.InvariantCulture
+  let anyNumber = NumberStyles.Any
+  let invariantCulture = CultureInfo.InvariantCulture
 
   let NaN = Double.NaN
   let NegInf = Double.NegativeInfinity 
   let PosInf = Double.PositiveInfinity
+
+module Operators =
+  let inline (<==) a b = a := b !a
+  let inline (|!>) a b = b !a 
+  let inline (|?>) a b = Option.map b a
+  let inline (|?) a b = match a with Some a -> a | _ -> b()

84bbd8220195652c543e6b7c087f0fdc1d19ed35
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-10-23 09:39:35 +0200

    Added missing files

---
+++ Src/Core/IronJS/Operators.fs
@@ -0,0 +1,7 @@
+﻿namespace IronJS
+
+module Operators =
+  let inline (<==) a b = a := b !a
+  let inline (|!>) a b = b !a 
+  let inline (|?>) a b = Option.map b a
+  let inline (|?) a b = match a with Some a -> a | _ -> b()

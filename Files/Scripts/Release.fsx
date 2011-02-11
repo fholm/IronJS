e8c49c3b03949f87681613723984d15b8bb42d58
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2011-02-11 10:57:51 +0100

    More moving around

--- Files/Scripts/Release.fsx
+++ Files/Scripts/Release.fsx
@@ -3,10 +3,8 @@
 #r @"../../Lib/Antlr3.Runtime.dll"
 #r @"../../Lib/CLR4/Xebic.ES3.dll"
 #r @"../../Lib/CLR4/Microsoft.Dynamic.dll"
-#r @"../../Src/Dependencies/FSKit/Src/bin/Release/FSKit.dll"
-#r @"../../Src/Core/IronJS/bin/Release/IronJS.dll"
-#r @"../../Src/Core/IronJS.Compiler/bin/Release/IronJS.Compiler.dll"
-#r @"../../Src/Core/IronJS.Runtime/bin/Release/IronJS.Runtime.dll"
+#r @"../../Src/FSKit/Src/bin/Release/FSKit.dll"
+#r @"../../Src/IronJS/bin/Release/IronJS.dll"
 
 open System
 open System.Reflection
--- Tests/Operators.fsx
+++ Tests/Operators.fsx
@@ -3,10 +3,8 @@
 #r @"../Lib/Antlr3.Runtime.dll"
 #r @"../Lib/CLR4/Microsoft.Dynamic.dll"
 #r @"../Lib/CLR4/Xebic.ES3.dll"
-#r @"../Src/Dependencies/FSKit/Src/bin/Debug/FSKit.dll"
-#r @"../Src/Core/IronJS/bin/Debug/IronJS.dll"
-#r @"../Src/Core/IronJS.Compiler/bin/Debug/IronJS.Compiler.dll"
-#r @"../Src/Core/IronJS.Runtime/bin/Debug/IronJS.Runtime.dll"
+#r @"../Src/FSKit/Src/bin/Debug/FSKit.dll"
+#r @"../Src/IronJS/bin/Debug/IronJS.dll"
 
 open IronJS
 open IronJS.Aliases

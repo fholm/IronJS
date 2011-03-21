dfcd5a31774f5affd47d6f96165509fd187f702c
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2011-03-21 18:49:41 +0100

    Changelist update, small changes

--- Src/Tools/REPL/Program.fs
+++ Src/Tools/REPL/Program.fs
@@ -8,37 +8,11 @@ module Main =
   open IronJS.Compiler.Core
 
   let main () =
-    #if DEBUG
-    #else
-    System.Threading.Thread.CurrentThread.Priority <- System.Threading.ThreadPriority.Highest
-    #endif
+    System.Threading.Thread.CurrentThread.Priority 
+      <- System.Threading.ThreadPriority.Highest
 
     let ctx = IronJS.Hosting.Context.Create()
-    let src = @"function foo(a, b) {
-	      var c = 3;
 
-	      function bar() {
-		      return a + b + c + d;
-	      }
-
-	      function zoo() {  
-		      if(true) {
-			      return false;
-		      } else {
-			      return true;
-		      }
-	      }
-
-	      return bar() + zoo();
-      }"
-
-    let ast = src |> IronJS.Compiler.Parser.parseString ctx.Environment
-
-    FSKit.Perf.time "IronJS" (fun () ->
-      for i = 0 to 10000 do
-         src |> IronJS.Compiler.Parser.parseString ctx.Environment |> ignore
-    )
-
-    Console.ReadLine() |> ignore
+    ()
 
   main() |> ignore

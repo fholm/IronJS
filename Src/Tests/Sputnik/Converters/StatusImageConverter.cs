53a1bfe93720bfccd2455e6150323d4a0d7fa58c
John Gietzen <otac0n@gietzen.us>
2011-03-28 06:28:18 -0500

    Added missing image converter, and changed .gitignore to exclude bin & obj directories, while allowing *.cs files.
    Signed-off-by: John Gietzen <otac0n@gietzen.us>

---
+++ Src/Tests/Sputnik/Converters/StatusImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace IronJS.Tests.Sputnik.Converters
+{
+    public class StatusImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return "Images/" + value.ToString().ToLower() + ".gif";
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

11b9da000168aff57e4853ccc7b944ce3dd6c263
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2011-03-28 10:17:55 +0200

    Trying to isolate the memory corruption error

--- Files/Scripts/Debug.fsx
+++ Files/Scripts/Debug.fsx
@@ -12,11 +12,35 @@ let ctx = Hosting.Context.Create()
 ctx.SetupPrintFunction()
 
 let src = @"
-  function foo(a, b) {
-    return arguments.length;
-  }
+// CHECK#1
+if (Object(true).valueOf() !== true){
+  $ERROR('#1: Object(true).valueOf() === true. Actual: ' + (Object(true).valueOf()));
+}
 
-  foo(2);
+// CHECK#2
+if (typeof Object(true) !== 'object'){
+  $ERROR('#2: typeof Object(true) === object. Actual: ' + (typeof Object(true)));
+}
+
+// CHECK#3
+if (Object(true).constructor.prototype !== Boolean.prototype){
+  $ERROR('#3: Object(true).constructor.prototype === Boolean.prototype. Actual: ' + (Object(true).constructor.prototype));
+}
+
+// CHECK#4
+if (Object(false).valueOf() !== false){
+  $ERROR('#4: Object(false).valueOf() === false. Actual: ' + (Object(false).valueOf()));
+}
+
+// CHECK#5
+if (typeof Object(false) !== 'object'){
+  $ERROR('#5: typeof Object(false) === object. Actual: ' + (typeof Object(false)));
+}
+
+// CHECK#6
+if (Object(false).constructor.prototype !== Boolean.prototype){
+  $ERROR('#6: Object(false).constructor.prototype === Boolean.prototype. Actual: ' + (Object(false).constructor.prototype));
+}
 " 
 
 src |> ctx.Execute

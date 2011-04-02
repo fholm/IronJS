13a9d1e0117741a3a9e10868f46fc52e4dd15965
John Gietzen <otac0n@gietzen.us>
2011-04-01 20:43:52 -0500

    Added UnitTest framework, with a dependency on NUnit. Made heavy use of TestCases, to cut down on the size of the tests.
    Signed-off-by: John Gietzen <otac0n@gietzen.us>

---
+++ Src/Tests/UnitTests/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace IronJS.Tests.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Reflection;
+    using NUnit.Framework;
+    using NUnit.ConsoleRunner;
+
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Runner.Main(new[] { Program.GetAssemblyPath() });
+        }
+
+        public static string GetAssemblyPath()
+        {
+            return new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+        }
+    }
+}

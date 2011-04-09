5c6e775c501048bdb6347a903670c47ea3be9bbf
John Gietzen <otac0n@gietzen.us>
2011-04-09 10:11:48 -0500

    Implemented "GetSpecialInclude" in a similar fashion to sputnik.py.
    Signed-off-by: John Gietzen <otac0n@gietzen.us>

--- Src/Tests/Sputnik/MainWindow.xaml.cs
+++ Src/Tests/Sputnik/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace IronJS.Tests.Sputnik
     {
         private BackgroundWorker worker = new BackgroundWorker();
         private string libPath;
+        private static readonly Dictionary<string, string> includeCache = new Dictionary<string, string>();
         private volatile bool showExprTrees;
 
         private IList<TestGroup> testGroups;
@@ -59,7 +60,7 @@ namespace IronJS.Tests.Sputnik
             this.worker.WorkerSupportsCancellation = true;
 
             var rootPath = Path.Combine(new DirectoryInfo(GetExecutableDirectory()).Parent.Parent.FullName, "sputnik-v1");
-            this.libPath = Path.Combine(rootPath, "lib");
+            libPath = Path.Combine(rootPath, "lib");
             var testsPath = Path.Combine(rootPath, "tests");
 
             this.rootTestGroup = new TestGroup(null, null)
@@ -285,13 +286,65 @@ namespace IronJS.Tests.Sputnik
             Process.Start(info);
         }
 
+        private static string GetInclude(string libPath, string file)
+        {
+            string source;
+
+            if (!includeCache.TryGetValue(file, out source))
+            {
+                source = GetSpecialInclude(file);
+                if (source == null)
+                {
+                    source = File.ReadAllText(Path.Combine(libPath, file));
+                }
+
+                includeCache.Add(file, source);
+            }
+
+            return source;
+        }
+
+        private static string GetSpecialInclude(string file)
+        {
+            if (file == "environment.js")
+            {
+                return GetTimeZoneInfoInclude();
+            }
+
+            return null;
+        }
+
+        private static string GetTimeZoneInfoInclude()
+        {
+            var local = TimeZoneInfo.Local;
+            var now = DateTime.UtcNow;
+
+            var daylightRule = local.GetAdjustmentRules().Where(a => a.DateEnd > now && a.DateStart <= now).Single();
+
+            var start = daylightRule.DaylightTransitionStart;
+            var end = daylightRule.DaylightTransitionEnd;
+
+            var info = new StringBuilder();
+            info.AppendLine(string.Format("var $DST_end_hour = {0};", end.TimeOfDay.Hour));
+            info.AppendLine(string.Format("var $DST_end_minutes = {0};", end.TimeOfDay.Minute));
+            info.AppendLine(string.Format("var $DST_end_month = {0};", end.Month));
+            info.AppendLine(string.Format("var $DST_end_sunday = '{0}';", end.Day > 15 ? "last" : "first"));
+            info.AppendLine(string.Format("var $DST_start_hour = {0};", start.TimeOfDay.Hour));
+            info.AppendLine(string.Format("var $DST_start_minutes = {0};", start.TimeOfDay.Minute));
+            info.AppendLine(string.Format("var $DST_start_month = {0};", start.Month));
+            info.AppendLine(string.Format("var $DST_start_sunday = '{0}';", start.Day > 15 ? "last" : "first"));
+            info.AppendLine(string.Format("var $LocalTZ = {0};", local.BaseUtcOffset.TotalSeconds / 3600));
+
+            return info.ToString();
+        }
+
         private static IronJS.Hosting.CSharp.Context CreateContext(string libPath, Action<string> errorAction)
         {
             var ctx = new IronJS.Hosting.CSharp.Context();
 
             Action<string> failAction = error => { throw new Exception(error); };
             Action<string> printAction = message => Debug.WriteLine(message);
-            Action<string> includeAction = file => ctx.ExecuteFile(Path.Combine(libPath, file));
+            Action<string> includeAction = file => ctx.Execute(GetInclude(libPath, file));
 
             var errorFunc = IronJS.Native.Utils.CreateFunction(ctx.Environment, 1, errorAction);
             var failFunc = IronJS.Native.Utils.CreateFunction(ctx.Environment, 1, failAction);
--- Src/Tests/UnitTests/Sputnik/SputnikTestFixture.cs
+++ Src/Tests/UnitTests/Sputnik/SputnikTestFixture.cs
@@ -1,8 +1,10 @@
 ﻿namespace IronJS.Tests.UnitTests.Sputnik
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using NUnit.Framework;
 
@@ -10,6 +12,7 @@
     {
         private static readonly string basePath;
         private static readonly string libPath;
+        private static readonly Dictionary<string, string> includeCache = new Dictionary<string, string>();
         private readonly string testsPath;
 
         static SputnikTestFixture()
@@ -27,13 +30,65 @@
             this.testsPath = Path.Combine(basePath, testsPath);
         }
 
+        private static string GetInclude(string file)
+        {
+            string source;
+
+            if (!includeCache.TryGetValue(file, out source))
+            {
+                source = GetSpecialInclude(file);
+                if (source == null)
+                {
+                    source = File.ReadAllText(Path.Combine(libPath, file));
+                }
+
+                includeCache.Add(file, source);
+            }
+
+            return source;
+        }
+
+        private static string GetSpecialInclude(string file)
+        {
+            if (file == "environment.js")
+            {
+                return GetTimeZoneInfoInclude();
+            }
+
+            return null;
+        }
+
+        private static string GetTimeZoneInfoInclude()
+        {
+            var local = TimeZoneInfo.Local;
+            var now = DateTime.UtcNow;
+
+            var daylightRule = local.GetAdjustmentRules().Where(a => a.DateEnd > now && a.DateStart <= now).Single();
+
+            var start = daylightRule.DaylightTransitionStart;
+            var end = daylightRule.DaylightTransitionEnd;
+
+            var info = new StringBuilder();
+            info.AppendLine(string.Format("var $DST_end_hour = {0};", end.TimeOfDay.Hour));
+            info.AppendLine(string.Format("var $DST_end_minutes = {0};", end.TimeOfDay.Minute));
+            info.AppendLine(string.Format("var $DST_end_month = {0};", end.Month));
+            info.AppendLine(string.Format("var $DST_end_sunday = '{0}';", end.Day > 15 ? "last" : "first"));
+            info.AppendLine(string.Format("var $DST_start_hour = {0};", start.TimeOfDay.Hour));
+            info.AppendLine(string.Format("var $DST_start_minutes = {0};", start.TimeOfDay.Minute));
+            info.AppendLine(string.Format("var $DST_start_month = {0};", start.Month));
+            info.AppendLine(string.Format("var $DST_start_sunday = '{0}';", start.Day > 15 ? "last" : "first"));
+            info.AppendLine(string.Format("var $LocalTZ = {0};", local.BaseUtcOffset.TotalSeconds / 3600));
+
+            return info.ToString();
+        }
+
         private static IronJS.Hosting.CSharp.Context CreateContext(Action<string> errorAction)
         {
             var ctx = new IronJS.Hosting.CSharp.Context();
 
             Action<string> failAction = error => Assert.Fail(error);
             Action<string> printAction = message => Trace.WriteLine(message);
-            Action<string> includeAction = file => ctx.ExecuteFile(Path.Combine(libPath, file));
+            Action<string> includeAction = file => ctx.Execute(GetInclude(file));
 
             var errorFunc = IronJS.Native.Utils.CreateFunction(ctx.Environment, 1, errorAction);
             var failFunc = IronJS.Native.Utils.CreateFunction(ctx.Environment, 1, failAction);

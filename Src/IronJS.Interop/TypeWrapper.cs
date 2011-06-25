657259d349d30175ad0e7df778cc857d37ec915e
John Gietzen <otac0n@gietzen.us>
2011-06-25 12:24:17 -0500

    Added a few overrides for use in the type wrapper.

--- Src/IronJS.Interop/TypeWrapper.cs
+++ Src/IronJS.Interop/TypeWrapper.cs
@@ -15,6 +15,29 @@ namespace IronJS.Interop
             this.type = type;
         }
 
+        public override BoxedValue Get(string name)
+        {
+            return base.Get(name);
+        }
+
+        public override bool Has(string name)
+        {
+            return base.Has(name);
+        }
+
+        public override bool HasOwn(string name)
+        {
+            return base.HasOwn(name);
+        }
+
+        public override string ClassName
+        {
+            get
+            {
+                return this.type.FullName;
+            }
+        }
+
         public static TypeWrapper Create(Environment env, Type type)
         {
             var prototype = (CommonObject)null;

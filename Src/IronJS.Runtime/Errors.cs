9a72301003ac535e185e6833fdd55e572be9e3cb
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2011-07-04 22:56:42 +0200

    Ported Error, CompilerError and RuntimeError classes. Without static Raise methods

---
+++ Src/IronJS.Runtime/Errors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronJS.Runtime
+{
+    public abstract class Error : Exception
+    {
+        public Error(string message)
+            : base(message)
+        {
+
+        }
+    }
+
+    public class CompilerError : Error
+    {
+        public Tuple<int, int> Position { get; private set; }
+        public string Source { get; private set; }
+        public string Path { get; private set; }
+
+        public CompilerError(string message, Tuple<int, int> position, string source, string path)
+            : base(message)
+        {
+            Position = position ?? Tuple.Create(0, 0);
+            Source = source ?? "<unknown>";
+            Path = path ?? "<unknown>";
+        }
+    }
+
+    public class RuntimeError : Error
+    {
+        public RuntimeError(string message)
+            : base(message)
+        {
+
+        }
+    }
+}

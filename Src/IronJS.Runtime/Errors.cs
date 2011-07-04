e4c3d259be3f8985e1c540b63811a92b8b76ad2a
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2011-07-04 23:07:52 +0200

    Started porting over SourceCodePrettyPrinter to C#

--- Src/IronJS.Runtime/Errors.cs
+++ Src/IronJS.Runtime/Errors.cs
@@ -5,10 +5,40 @@ using System.Text;
 
 namespace IronJS.Runtime
 {
+    internal static class SourceCodePrinter
+    {
+        private static string[] SplitLines(string input)
+        {
+            var cleanedInput = (input ?? "").Replace("\r\n", "\n").Replace("\r", "\n");
+            return System.Text.RegularExpressions.Regex.Split(cleanedInput, "\n");
+        }
+
+        private static string LineNumber(int padding, int input)
+        {
+            return (input.ToString()).PadLeft(padding, '0');
+        }
+
+        private static string MakeArrow(int length)
+        {
+            var builder = new StringBuilder(length).Insert(0, "-", length);
+            return builder + "^";
+        }
+
+        internal string PrettyPrintSourceError(Tuple<int, int> aboveBelow, Tuple<int, int> lineCol, string source)
+        {
+            var above = aboveBelow.Item1;
+            var below = aboveBelow.Item2;
+            var line = lineCol.Item1;
+            var column = lineCol.Item2;
+
+            throw new NotImplementedException();
+        }
+    }
+
     public abstract class Error : Exception
     {
         public Error(string message)
-            : base(message)
+            : base(message ?? "<unknown>")
         {
 
         }

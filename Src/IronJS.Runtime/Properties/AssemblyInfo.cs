8ae97dcfcf2d49483d11efdf3f0bb71ef302f6da
John Gietzen <otac0n@gietzen.us>
2011-07-04 12:08:11 -0500

    Added a new assembly `IronJS.Runtime` to hold the non-compiler parts of IronJS.

---
+++ Src/IronJS.Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IronJS")]
+[assembly: AssemblyDescription("IronJS - A JavaScript runtime for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IronJS")]
+[assembly: AssemblyProduct("IronJS")]
+[assembly: AssemblyCopyright("Copyright © Fredrik Holmström, 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("93e32fd4-5c93-4d0b-91d5-0b4e54f0ce2d")]
+
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]

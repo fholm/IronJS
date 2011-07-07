d6da7b9c7c3a2fa7382d1de0aec4129d0a0c31ee
John Gietzen <otac0n@gietzen.us>
2011-07-06 21:23:06 -0500

    Finished porting `SparseArray` and moved it to an internal class of `ArrayObject`.

--- Src/IronJS.Runtime/Objects/ArrayObject.cs
+++ Src/IronJS.Runtime/Objects/ArrayObject.cs
@@ -9,42 +9,138 @@ namespace IronJS.Runtime
     {
         public const uint DenseMaxIndex = 2147483646u;
         public const uint DenseMaxSize = 2147483647u;
-    }
 
-    //TODO: Not done yet
-    public class SparseArray
-    {
-        SortedDictionary<uint, BoxedValue> storage =
-            new SortedDictionary<uint, BoxedValue>();
 
-        public SortedDictionary<uint, BoxedValue> Members
+        public class SparseArray
         {
-            get { return storage; }
-        }
+            SortedDictionary<uint, BoxedValue> storage =
+                new SortedDictionary<uint, BoxedValue>();
 
-        public void Put(uint index, BoxedValue value)
-        {
-            storage[index] = value;
-        }
+            public SortedDictionary<uint, BoxedValue> Members
+            {
+                get { return this.storage; }
+            }
 
-        public bool Has(uint index)
-        {
-            return storage.ContainsKey(index);
-        }
+            public void Put(uint index, BoxedValue value)
+            {
+                this.storage[index] = value;
+            }
 
-        public BoxedValue Get(uint index)
-        {
-            return storage[index];
-        }
+            public bool Has(uint index)
+            {
+                return this.storage.ContainsKey(index);
+            }
 
-        public bool TryGet(uint index, out BoxedValue value)
-        {
-            return storage.TryGetValue(index, out value);
-        }
+            public BoxedValue Get(uint index)
+            {
+                return this.storage[index];
+            }
 
-        public bool Remove(uint index)
-        {
-            return storage.Remove(index);
+            public bool TryGet(uint index, out BoxedValue value)
+            {
+                return this.storage.TryGetValue(index, out value);
+            }
+
+            public bool Remove(uint index)
+            {
+                return this.storage.Remove(index);
+            }
+
+            public void PutLength(uint newLength, uint length)
+            {
+                if (newLength >= length)
+                {
+                    return;
+                }
+
+                foreach (var key in this.storage.Keys)
+                {
+                    if (key >= newLength)
+                        this.storage.Remove(key);
+                }
+            }
+
+            public void Shift()
+            {
+                this.storage.Remove(0);
+
+                foreach (var key in this.storage.Keys)
+                {
+                    var value = this.storage[key];
+                    this.storage.Remove(key);
+                    this.storage.Add(key, value); //FIXME: Isn't this supposed to subtract one from the key?  This probably isn't caught by our tests, because they don't trigger sparse arrays.
+                }
+            }
+
+            public void Reverse(uint length)
+            {
+                var newStorage = new SortedDictionary<uint, BoxedValue>();
+
+                foreach (var kvp in this.storage)
+                {
+                    newStorage.Add(length - kvp.Key - 1, kvp.Value);  //FIXME: What do we do when length > max(key)?  Does this just allow negative indices?
+                }
+
+                this.storage = newStorage;
+            }
+
+            public void Sort(Comparison<BoxedValue> comparison)
+            {
+                var sorted = this.storage.Values.ToArray();
+                Array.Sort(sorted, comparison);
+
+                this.storage.Clear();
+                for (uint i = 0; i < sorted.Length; i++)
+                {
+                    this.storage.Add(i, sorted[i]);
+                }
+            }
+
+            public void Unshift(BoxedValue[] args)
+            {
+                var length = (uint)args.Length;
+
+                var newStorage = new SortedDictionary<uint, BoxedValue>();
+
+                foreach (var kvp in this.storage)
+                {
+                    newStorage.Add(kvp.Key + length, kvp.Value);
+                }
+
+                for (uint i = 0; i < length; i++)
+                {
+                    newStorage.Add(i, args[i]);
+                }
+
+                this.storage = newStorage;
+            }
+
+            public void GetAllIndexProperties(IDictionary<uint, BoxedValue> dict, uint length)
+            {
+                foreach (var kvp in this.storage)
+                {
+                    if ((kvp.Key < length) && !dict.ContainsKey(kvp.Key))
+                    {
+                        dict.Add(kvp.Key, kvp.Value);
+                    }
+                }
+            }
+
+            public static SparseArray OfDense(Descriptor[] values)
+            {
+                var sparse = new SparseArray();
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (values[i].HasValue)
+                    {
+                        uint num = (uint)i;
+                        sparse.storage[num] = values[i].Value;
+                    }
+                }
+
+                return sparse;
+            }
         }
     }
 }

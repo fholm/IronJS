fd113e9d63971b6945c19501768f7bc1e00d83cd
John Gietzen <otac0n@gietzen.us>
2011-07-09 00:11:30 -0500

    Changed the packaging script to build the solution files directly.

--- Package/package.cmd
+++ Package/package.cmd
@@ -1,10 +1,4 @@
 @echo off
 set msbuild=%systemroot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
-set target=%1
-if "%target%"=="" set target=Build
-
-set platform=%2
-if "%platform%"=="" set platform=AnyCPU
-
-"%msbuild%" Package.msbuild /t:%target% /p:Platform=%platform% || pause
+"%msbuild%" Package.msbuild || pause

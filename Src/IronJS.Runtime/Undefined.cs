fa75cfef264dbb1213067e7e9ebda0582b3e2eea
John Gietzen <otac0n@gietzen.us>
2011-07-10 11:35:10 -0500

    Added some dynamic operator overloads to the undefined object.

--- Src/IronJS.Runtime/Undefined.cs
+++ Src/IronJS.Runtime/Undefined.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Dynamic;
+using System.Linq.Expressions;
 
 namespace IronJS.Runtime
 {
-    public class Undefined
+    public class Undefined : DynamicObject
     {
         private static readonly Undefined instance;
         private static readonly BoxedValue boxed;
@@ -30,6 +32,33 @@ namespace IronJS.Runtime
         public override string ToString()
         {
             return "undefined";
+        }
+
+        public override bool TryBinaryOperation(BinaryOperationBinder binder, object arg, out object result)
+        {
+            switch (binder.Operation)
+            {
+                case ExpressionType.Add:
+                    result = Operators.add(Undefined.boxed, BoxedValue.Box(arg)).UnboxObject();
+                    return true;
+            }
+
+            return base.TryBinaryOperation(binder, arg, out result);
+        }
+
+        public override bool TryUnaryOperation(UnaryOperationBinder binder, out object result)
+        {
+            switch (binder.Operation)
+            {
+                case ExpressionType.UnaryPlus:
+                    result = Operators.plus(Undefined.boxed).UnboxObject();
+                    return true;
+                case ExpressionType.Negate:
+                    result = Operators.minus(Undefined.boxed).UnboxObject();
+                    return true;
+            }
+
+            return base.TryUnaryOperation(binder, out result);
         }
     }
 }
